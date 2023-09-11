af96b743ef3aff390927f7897b230f6e81ed5105
Sumanth <89512217+sumanth-appu@users.noreply.github.com>
2023-09-11 23:08:34 +0530

    Update1 app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,59 +1,63 @@
-const defaultnum=0 ;
+
+const defaultnum = 0;
 let currentResult = defaultnum;
-let logentries =[];
-function getUserInput(){
-  return  parseInt(userInput.value);
+let logentries = [];
+function getUserInput() {
+  return parseInt(userInput.value);
 }
 // calculate the reult and write on user log
-function writeoutput(operator,resultbefore,resultafter){
-  const calcDescription= `${resultbefore}${operator}${resultafter}`
+function writeoutput(operator, resultbefore, resultafter) {
+  const calcDescription = `${resultbefore}${operator}${resultafter}`;
   outputResult(currentResult, calcDescription);
-  return (calcDescription)
+  return calcDescription;
 }
-function writelog(
-  operation,
-  previousResult,
-  opernum
-){
-  const logentr ={
-    operation:operation,
-    previousresult:previousResult,
-    num:opernum
-   }
-   logentries.push(logentr);
-   console.log(logentries);
+//write value on the display
+function writelog(operation, previousResult, opernum) {
+  const logentr = {
+    operation: operation,
+    previousresult: previousResult,
+    num: opernum,
+  };
+  logentries.push(logentr);
+  console.log(logentries);
 }
-function add() {
-  const Input=getUserInput();
- const inireuslt=currentResult;
- currentResult += Input;
- writeoutput('+',inireuslt,Input);
- writelog("Add",inireuslt,Input);
+// calculation function with conditional statements
+function calculate(Type) {
+  const Input = getUserInput();
+  const inireuslt = currentResult;
+  let mathopr;
+  if (Type === "Add") {
+    currentResult += Input;
+    mathopr = "+";
+  } else if (Type === "Subtract") {
+    currentResult -= Input;
+    mathopr = "-";
+  }else if(Type==="MULTLIPY"){
+    currentResult *= Input;
+    mathopr = "*";
+  }else if(Type==="DIVIDE"){
+    currentResult /= Input;
+    mathopr ="/";
+  }
 
- 
+  writeoutput(mathopr, inireuslt, Input);
+  writelog(Type, inireuslt, Input);
 }
-function subtract(){
-  const Input=getUserInput();
-  const inireuslt=currentResult;
-  currentResult -= Input;
-  writeoutput('-',inireuslt,Input);
-  writelog("SUBTRACT",inireuslt,Input);
+
+//  Different Functions for calculation
+function add() {
+  calculate("Add");
 }
-function multlipy(){
-  const Input=getUserInput();
-  const inireuslt=currentResult;  
-  currentResult *=  Input;
-  writeoutput('*',inireuslt,Input);
-  writelog("MULTLIPY",inireuslt,Input);
+function subtract() {
+  calculate("Subtract");
 }
-function divide(){
-  const Input=getUserInput();
-  const inireuslt=currentResult;
-  currentResult /= Input;
-  writeoutput('/',inireuslt,Input);
-  writelog("DIVIDE",inireuslt,Input);
+function multlipy() {
+  calculate("MULTIPLY");
+}
+function divide() {
+calculate("DIVIDE");
 }
 addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click",subtract);
-multiplyBtn.addEventListener("click",multlipy);
-divideBtn.addEventListener("click",divide);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multlipy);
+divideBtn.addEventListener("click", divide);
